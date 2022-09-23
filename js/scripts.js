828d7b2cc3b9fc91a91004d54a885d4edfaba045
cknm-cmd <75062776+cknm-cmd@users.noreply.github.com>
2022-09-23 21:13:58 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -214,7 +214,8 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== '3fe6b0e23ae113bc9c405236054e9a26' {
+        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
+            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
             $.post('https://script.google.com/macros/s/AKfycbzeG1_VX29YOcpPDkyo9hj4YaaQ3Vby_Uf9U7XHxybaGnnBJNf-xcgX_N3HiEA5V9nkgQ/exec', data)
